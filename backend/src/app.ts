b9b26d0e3d129121cad109e05a9ad337caa1c25f
Christian Alexsander <christianalexbh@hotmail.com>
2020-01-20 17:15:47 -0300

    changes and ident

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import app from './server';
-import { BACKEND_PORT, HOSTNAME } from './config/env';
+import app from "./server";
+import { BACKEND_PORT, HOSTNAME } from "./config/env";
 
 const host = `http://${HOSTNAME}:${BACKEND_PORT}`;
 
